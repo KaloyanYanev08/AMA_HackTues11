3404f7282fb9b1b7855ae0197500dda01f86792e
JukFiuuTech <108091130+jukfiuune@users.noreply.github.com>
2025-03-21 14:55:18 +0200

    Move most of the functionality to the backend

--- ai_client.py
+++ ai_client.py
@@ -6,33 +6,25 @@ from re import DOTALL
 from config import ollama_model, ollama_endpoint
 
 def send_to_model(prompt):
-    try:
-        client = Client(
-            host=ollama_endpoint
-        )
-        response = dict(client.chat(
-            model=ollama_model,
-            messages=[{'role': 'user', 'content': prompt}],
-            stream=False
-        ))
+    client = Client(
+        host=ollama_endpoint
+    )
+    response = dict(client.chat(
+        model=ollama_model,
+        messages=[{'role': 'user', 'content': prompt}],
+        stream=False
+    ))
+
+    if not response["done"] or response["done_reason"] != 'stop':
+        raise ResponseError("Response stopped prematurely")
         
-        print(response)
+    end_of_reason = regex(r"\s*</think>\s*(.*)", response["message"]["content"], DOTALL)
+    json_resp = ""
+    if end_of_reason:
+        json_resp = end_of_reason.group(1).replace("```json", "").replace("```", "").strip()
+    else:
+        raise ResponseError("Response format is incorrect")
 
-        print("\n\n\n\n\n\n\n")
+    from json import loads
 
-        if not response["done"] or response["done_reason"] != 'stop':
-            raise ResponseError("Response stopped prematurely")
-        
-        end_of_reason = regex(r"\s*</think>\s*(.*)", response["message"]["content"], DOTALL)
-        json_resp = ""
-        if end_of_reason:
-            json_resp = end_of_reason.group(1).replace("```json", "").replace("```", "").strip()
-        else:
-            raise ResponseError("Response format is incorrect")
-
-        return Response(
-            json_resp,
-            mimetype='application/json'
-        )
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    return loads(json_resp)
